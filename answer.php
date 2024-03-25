ea9303a3fcf0e2f67eb7745fbf9371fd566692f8
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-25 23:26:51 +0000

    created answer.php

---
+++ answer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Area and Perimeter of Rectangle, in PHP" />
+    <meta name="keywords" content="mths, ics2o" />
+    <meta name="author" content="Mr. Coxall" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="manifest" href="site.webmanifest" />
+    <title>Area and Perimeter of Rectangle,in PHP</title>
+  </head>
+  <body>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">Area of a Triangle, in PHP</span>
+        </div>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="right-image">
+          <img src="./images/area_of_trinagle.png" alt="area of a triangle" width="250" />
+        </div>
+        <div class="page-content-php">
+          <div id="user-info">
+            <?php
+            $baseOfTriangle = $_POST["base-of-triangle"];
+            $heightOfTriangle = $_POST["height-of-triangle"];
+
+            // process
+            $area = $baseOfTriangle * $heightOfTriangle * 0.5;
+
+            // output
+            echo "If a Triangle has base = " . $baseOfTriangle . " cm and the height = " . $heightOfTriangle . " cm:";
+            echo "<br />";
+            echo "<br />";
+            echo "The area of the triangle is " . $area . " cm².";
+            echo "<br />";
+            ?>
+          </div>
+        </div>
+      </main>
+    </div>
+  </body>
+</html>
