bb3a9fb663cc896f2f20f6c1f4f678453381a692
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-25 23:39:25 +0000

    added last name

--- answer.php
+++ answer.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="description" content="Area of a Triangle, in PHP" />
     <meta name="keywords" content="mths, ics2o" />
-    <meta name="author" content="Nathan" />
+    <meta name="author" content="Nathan De Silva" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./css/style.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
